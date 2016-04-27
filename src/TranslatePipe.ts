0e6eb5a6b34c3879567046445ce84272561d95d8
apoterenko <a.poterenko@gmail.com>
2016-04-27 14:38:22 +0300

    Added TranslatePipe

---
+++ src/TranslatePipe.ts
@@ -0,0 +1,20 @@
+import {
+    Inject,
+    Pipe,
+    PipeTransform
+} from 'angular2/core';
+
+import {TranslateI18Next} from './TranslateI18Next';
+
+@Pipe({
+    name: 'translate'
+})
+export class TranslatePipe implements PipeTransform {
+
+    constructor(@Inject(TranslateI18Next) private translateI18Next:TranslateI18Next) {
+    }
+
+    public transform(value:any, args:any[]):string {
+        return this.translateI18Next.translate(value, args && args.length ? args[0] : null);
+    }
+}
