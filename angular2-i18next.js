c91bdfcf4de5170fd00beff4a870df73b602fd1f
apoterenko <a.poterenko@gmail.com>
2016-04-27 18:01:40 +0300

    *added angular2-i18next.js

---
+++ angular2-i18next.js
@@ -0,0 +1,7 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+__export(require('./src/TranslateI18Next'));
+__export(require('./src/TranslatePipe'));
+//# sourceMappingURL=angular2-i18next.js.map
