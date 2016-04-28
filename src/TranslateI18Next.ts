6d6d124cbd84f89fc9f19ec3579737507b033c50
apoterenko <a.poterenko@gmail.com>
2016-04-28 15:52:51 +0300

    *improve TranslateI18Next: logging

--- src/TranslateI18Next.ts
+++ src/TranslateI18Next.ts
@@ -9,13 +9,27 @@ const i18next = require('i18next'),
     i18nextXHRBackend = require('i18next-xhr-backend'),
     i18nextBrowserLanguageDetector = require('i18next-browser-languagedetector');
 
+const DEFAULT_FALLBACK_LNG:string = 'en';
+
 @Injectable()
 export class TranslateI18Next {
 
     private ready:boolean = false;
     private i18nextPromise:Promise.Thenable<void>;
 
+    private fallbackLng:string = DEFAULT_FALLBACK_LNG;
+    private debug:boolean = false;
+
     public init(options?:any):Promise.Thenable<void> {
+        options = options || {};
+
+        this.fallbackLng = options.fallbackLng = options.fallbackLng || this.fallbackLng;
+        this.debug = options.debug = options.debug || this.debug;
+
+        if (this.debug) {
+            console.debug('Fallback language is', this.fallbackLng);
+        }
+
         return this.i18nextPromise =
             new Promise<void>((resolve:(thenableOrResult?:void | Promise.Thenable<void>) => void, reject:(error:any) => void) => {
                 i18next
@@ -25,14 +39,18 @@ export class TranslateI18Next {
                         Object.assign({
                             detection: {
                                 order: ['navigator']
-                            },
-                            fallbackLng: 'en'
+                            }
                         }, options),
                         (err:any) => {
                             if (err) {
                                 console.error(err);
+
                                 reject(err);
                             } else {
+                                if (this.debug) {
+                                    console.debug('The translations has been loaded for the current language', i18next.language);
+                                }
+
                                 this.ready = true;
                                 resolve(null);
                             }
@@ -41,6 +59,18 @@ export class TranslateI18Next {
     }
 
     public translate(key:string, options?:any):string {
-        return i18next.t(key, options || {});
+        options = options || {};
+        const translatedValue = i18next.t(key, options);
+
+        if (translatedValue) {
+            return translatedValue;
+        }
+
+        if (this.debug) {
+            console.warn('The translation is not found for the key', key, 'and language', i18next.language);
+        }
+
+        return i18next.t(key, Object.assign(options, {lng: this.fallbackLng}))
+            || key;
     }
 }
