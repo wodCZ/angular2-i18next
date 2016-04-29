e37b89d19a3ce583fd7e32d59a7fa1af3e601018
apoterenko <a.poterenko@gmail.com>
2016-04-29 15:11:55 +0300

    *prevent caching lang (the language can change state at runtime)

--- src/browser/LanguageDetectorAdapter.ts
+++ src/browser/LanguageDetectorAdapter.ts
@@ -5,14 +5,11 @@ export class LanguageDetectorAdapter {
     static toBrowserLanguageDetector(browserLanguageDetector:ILanguageDetector):{new():ILanguageDetector} {
         class LanguageDetector implements ILanguageDetector {
 
-            private currentLanguage:string;
-
             public init() {
-                this.currentLanguage = browserLanguageDetector.detect();
             }
 
             public detect():string {
-                return this.currentLanguage;
+                return browserLanguageDetector.detect();
             }
 
             public cacheUserLanguage() {
