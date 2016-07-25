cfa2e1da3750e0596ceddffb3e9af17aa32f4dbd
apoterenko <a.poterenko@gmail.com>
2016-07-25 15:16:59 +0300

    *added exports

--- src/angular2-i18next.ts
+++ src/angular2-i18next.ts
@@ -1,3 +1,4 @@
 export {ILanguageDetector} from './browser/ILanguageDetector';
+export {IBackendOptions} from './backend/IBackendOptions';
 export {TranslateI18Next} from './TranslateI18Next';
 export {TranslatePipe} from './TranslatePipe';
