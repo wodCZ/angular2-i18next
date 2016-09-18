7f72f57a77f49f40ecdf39246e5847821f2d4dc2
apoterenko <a.poterenko@gmail.com>
2016-09-18 14:31:09 +0300

    *improve LanguageDetectorAdapter - added logging

--- src/browser/LanguageDetectorAdapter.ts
+++ src/browser/LanguageDetectorAdapter.ts
@@ -1,18 +1,22 @@
+import {LoggerFactory, ILogger} from 'ts-smart-logger';
+
 import {ILanguageDetector} from './ILanguageDetector';
 
 export class LanguageDetectorAdapter {
 
+	private static logger: ILogger = LoggerFactory.makeLogger(LanguageDetectorAdapter);
+
     static toBrowserLanguageDetector(browserLanguageDetector:ILanguageDetector):{new():ILanguageDetector} {
         class LanguageDetector implements ILanguageDetector {
 
-            public init() {
-            }
+        	constructor() {
+		        LanguageDetectorAdapter.logger.debug('[$LanguageDetectorAdapter][constructor] LanguageDetector wrapper has been instantiated');
+	        }
 
             public detect():string {
-                return browserLanguageDetector.detect();
-            }
-
-            public cacheUserLanguage() {
+                const result:string =  browserLanguageDetector.detect();
+	            LanguageDetectorAdapter.logger.debug('[$LanguageDetectorAdapter][detect] Detect method returns the language as', result);
+	            return result;
             }
         }
 
