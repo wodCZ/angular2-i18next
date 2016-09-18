c800447491fe2729297a27d0356938e4a916bda3
apoterenko <a.poterenko@gmail.com>
2016-09-18 14:33:28 +0300

    *restore methods for LanguageDetector

--- src/browser/LanguageDetectorAdapter.ts
+++ src/browser/LanguageDetectorAdapter.ts
@@ -13,11 +13,17 @@ export class LanguageDetectorAdapter {
 		        LanguageDetectorAdapter.logger.debug('[$LanguageDetectorAdapter][constructor] LanguageDetector wrapper has been instantiated');
 	        }
 
+	        public init() {
+	        }
+
             public detect():string {
                 const result:string =  browserLanguageDetector.detect();
 	            LanguageDetectorAdapter.logger.debug('[$LanguageDetectorAdapter][detect] Detect method returns the language as', result);
 	            return result;
             }
+
+	        public cacheUserLanguage() {
+	        }
         }
 
         LanguageDetector['type'] = 'languageDetector';
