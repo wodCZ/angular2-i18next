3c337bbf4f7921124c44e32413b3d77344fabde9
Martin Janeček <admin@ikw.cz>
2017-03-22 20:47:00 +0000

    AoT compatibility fix

--- src/TranslatePipe.ts
+++ src/TranslatePipe.ts
@@ -15,7 +15,7 @@ export class TranslatePipe implements PipeTransform {
     constructor(@Inject(TranslateI18Next) private translateI18Next:TranslateI18Next) {
     }
 
-    public transform(value:string, args:any[]):string {
+    public transform(value:string, args?:any[]):string {
         return this.translateI18Next.translate(value, Array.isArray(args) && args.length ? args[0] : args);
     }
 }
